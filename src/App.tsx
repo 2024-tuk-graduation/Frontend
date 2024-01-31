eda679ebd8411879f184b62e4b4383e08aba9277
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-02-01 01:48:41 +0900

    feat : 전역 style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Home, Login } from "@/pages/index";
-
+import GlobalStyle from "./GlobalStyle";
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <GlobalStyle />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
