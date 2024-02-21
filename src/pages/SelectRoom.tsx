4f5fe44a8751dd763f1aa25084c15e11d0a49a3e
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-02-21 23:52:51 +0900

    feat : 네비게이션 바 생성

---
+++ src/pages/SelectRoom.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "@/components";
+import React from "react";
+
+const SelectRoom = () => {
+  return (
+    <div className="bg-container">
+      <div className="container">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default SelectRoom;
