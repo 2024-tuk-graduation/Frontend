76c0837877decba895f7d47b6305a3993c1f014d
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-02-22 00:08:01 +0900

    refactor : navbar 크기 변수로 설정

--- src/pages/SelectRoom.tsx
+++ src/pages/SelectRoom.tsx
@@ -1,11 +1,23 @@
 import { Navbar } from "@/components";
 import React from "react";
+import createIcon from "@/assets/images/createRoom.svg";
+import enterIcon from "@/assets/images/enterRoom.svg";
 
 const SelectRoom = () => {
   return (
     <div className="bg-container">
       <div className="container">
         <Navbar />
+        <div>
+          <button>
+            <div>
+              <img src={createIcon} alt={"방 생성하기"} />
+              <div>
+                <p></p>
+              </div>
+            </div>
+          </button>
+        </div>
       </div>
     </div>
   );
