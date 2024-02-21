6e54656387653523bba5f55e76b2a1dec3125d01
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-02-22 00:53:37 +0900

    feat : 선택 페이지 퍼블리싱 완료

--- src/pages/SelectRoom.tsx
+++ src/pages/SelectRoom.tsx
@@ -8,12 +8,21 @@ const SelectRoom = () => {
     <div className="bg-container">
       <div className="container">
         <Navbar />
-        <div>
-          <button>
-            <div>
-              <img src={createIcon} alt={"방 생성하기"} />
+        <div className="selectRoom-container">
+          <button className="selectRoom-btn">
+            <div className="selectRoom-btn-container">
+              <img className="selectRoom-btn-Icon" src={createIcon} alt={"방 생성하기"} />
               <div>
-                <p></p>
+                <p className="selectRoom-btn-Text">방 생성하기</p>
+              </div>
+            </div>
+          </button>
+
+          <button>
+            <div className="selectRoom-btn-container ">
+              <img className="selectRoom-btn-Icon" src={enterIcon} alt={"방 입장하기"} />
+              <div>
+                <p className="selectRoom-btn-Text">방 입장하기</p>
               </div>
             </div>
           </button>
