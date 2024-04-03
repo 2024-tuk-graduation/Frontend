da340b0ed3bc47064c6a8597cc4d81ba546f56a7
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-04-03 17:23:05 +0900

    fix : 에디터페이지 container  사이즈 조절

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -12,10 +12,12 @@ import { Memo } from "@/components/Editor";
 import ModeEditor from "@/components/Editor/ModeEditor";
 import WhiteBoard from "@/components/Editor/WhiteBoard";
 import { Navbar, QandA } from "@/components";
+import SearchSection from "@/components/Editor/SearchSection";
+import { useHeightState } from "@/store/editorSection";
 
 const Editor = () => {
   const { setPersonMenu } = useEditorMenuActions();
-
+  const height = useHeightState();
   return (
     <WebSocketConnnect>
       <div className="container editor">
@@ -39,7 +41,8 @@ const Editor = () => {
                 />
                 <ModeEditor />
               </div>
-              <div className="editor-WhiteBoard-QnA-area">
+              {/* <SearchSection ></SearchSection> */}
+              <div className="editor-WhiteBoard-QnA-area" style={{ height }}>
                 <WhiteBoard />
                 <QandA />
               </div>
