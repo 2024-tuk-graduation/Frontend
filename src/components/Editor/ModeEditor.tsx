e5dbf7b1093b51d89e15624dfd2a22bc787aac90
jihye9549 <105404542+jihye9549@users.noreply.github.com>
2024-07-09 20:53:44 +0900

    feat : 화면 캡쳐 기능 구현

--- src/components/Editor/ModeEditor.tsx
+++ src/components/Editor/ModeEditor.tsx
@@ -1,26 +1,48 @@
 import { useModeState } from "@/store/editorRoomInfoStore";
-import React from "react";
+import React, { useRef } from "react";
 import CodeEditor from "./Code/CodeEditor";
 import BlankCanvas from "./Blank/ BlankCanvas";
 import { PdfView } from ".";
 import FileItemTitle from "./FileItemTitle";
+import saveAs from "file-saver";
+import html2canvas from "html2canvas";
 
 const ModeEditor = () => {
   const mode = useModeState();
+  const captureRef = useRef<HTMLDivElement>(null);
+
+  const handleDownload = async () => {
+    if (!captureRef.current) return;
+
+    try {
+      const div = captureRef.current;
+      const canvas = await html2canvas(div, { scale: 2 });
+      canvas.toBlob((blob) => {
+        if (blob !== null) {
+          saveAs(blob, "result.png");
+        }
+      });
+    } catch (error) {
+      console.error("Error converting div to image:", error);
+    }
+  };
 
   return (
-    <div className="mode-editor-container">
-      <div className={`blank-container ${mode === "blank" ? "select" : ""}`}>
-        <FileItemTitle fileName="1" fileType="pdf" />
-        <BlankCanvas />
-      </div>
+    <div>
+      <button onClick={handleDownload}>캡쳐하기</button>
+      <div className="mode-editor-container" ref={captureRef}>
+        <div className={`blank-container ${mode === "blank" ? "select" : ""}`}>
+          <FileItemTitle fileName="1" fileType="pdf" />
+          <BlankCanvas />
+        </div>
 
-      <div style={mode === "code" ? {} : { display: "none" }}>
-        <CodeEditor />
-      </div>
+        <div style={mode === "code" ? {} : { display: "none" }}>
+          <CodeEditor />
+        </div>
 
-      <div className={`pdf-container ${mode === "pdf" ? "select" : ""}`}>
-        <PdfView />
+        <div className={`pdf-container ${mode === "pdf" ? "select" : ""}`}>
+          <PdfView />
+        </div>
       </div>
     </div>
   );
